81df7d7711db060614ab14525791546ba39e8bdd
Gregory Travis <greg.m.travis@gmail.com>
2020-08-21 16:58:32 -0400

    game deals cards

--- src/war.rs
+++ src/war.rs
@@ -9,6 +9,12 @@ struct Player {
   discard: Vec<i32>, 
 }
 
+impl Player {
+  pub fn new(cards: &[i32]) -> Player {
+    return Player { hand: cards.to_vec(), discard: Vec::new() };
+  }
+}
+
 #[derive(Debug)]
 struct Game {
   a: Player,
@@ -16,9 +22,15 @@ struct Game {
 }
 
 impl Game {
-  pub fn new() -> Player {
-    return Player { hand: Vec::new(), discard: Vec::new() };
+  pub fn new() -> Game {
+    let d = random_deck();
+    // It's random, so splitting it in half is as good as really dealing.
+    let (a_cards, b_cards) = d.split_at(d.len()/2);
+    return Game { a: Player::new(a_cards), b: Player::new(b_cards) };
   }
+
+  // pub fn play(&mut self) {
+  // }
 }
 
 fn deck() -> Vec<i32> {
@@ -40,14 +52,14 @@ fn random_deck() -> Vec<i32> {
 fn randomize(xs: &mut [i32]) {
   let mut from = vec![0; xs.len()];
   from.clone_from_slice(xs);
-  println!("{:?}", xs);
-  println!("{:?}", from);
+  //println!("{:?}", xs);
+  //println!("{:?}", from);
   // print_type_of(xs);
   // print_type_of(from);
   for i in 0..xs.len() {
     xs[i] = remove_random(&mut from);
-    println!("{:?}", xs);
-    println!("{:?}", from);
+    //println!("{:?}", xs);
+    //println!("{:?}", from);
   }
   assert!(from.len() == 0, "uhoh");
 }
@@ -61,12 +73,11 @@ fn remove_random(xs: &mut Vec<i32>) -> i32 {
   let mut rng = rand::thread_rng();
   let i = rng.gen_range(0, xs.len());
   let x = xs.remove(i);
-  println!("Remove {} at {}", x, i);
+  //println!("Remove {} at {}", x, i);
   return x;
 }
 
 pub fn demo() {
-  println!("{:?}", Game::new());
-  let d = random_deck();
-  println!("{:?}", d);
+  let g = Game::new();
+  println!("{:?}", g);
 }
